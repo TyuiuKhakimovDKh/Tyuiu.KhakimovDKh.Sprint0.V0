e9b71182d2420cb2dea8e88c77631946442ab6c3
Достонбек Хакимов <Tyuiu.KhakimovDkh@outlook.com>
2024-11-14 21:48:06 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.KhakimovDKh.Sprint0.Task0.V0/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.KhakimovDKh.Sprint0.Task0.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console .ReadLine();
+        }
+    }     
+}
